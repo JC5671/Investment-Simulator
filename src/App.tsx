ced90094333a75372e2f0e21532dc8ded398fb10
Jason Henry <jasonchristopher5671@gmail.com>
2025-09-26 20:13:06 -0700

    Initialized shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return <h2 className="text-2xl font-bold text-blue-800">Hello World</h2>;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
